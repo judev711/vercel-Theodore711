220645b54cd7375c2730ef916ea99c726b3a7942
judev711 <juniordev711@gmail.com>
2024-10-20 05:48:58 +0200

    configuration de mon vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        "/src/assets/images/Shirt1.png",
+        "/src/assets/images/Shirt1.png",
+        "/src/assets/images/Shirt1.png",
+        "/src/assets/images/Logo.png",
+        '/src/assets/images/Women1.jpg',
+        "/src/assets/images/Women2.jpg",
+        "/src/assets/images/Women3.jpg",
+        "/src/assets/images/Women4.jpg",
+        "/src/assets/images/Women5.jpg",
+      ], // Ajoutez cette ligne
+    },
+  },
+});
